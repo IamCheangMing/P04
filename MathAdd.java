166398960765661bae718a366d400fa2349f3439
161110M CHEO CHEANG MING <161110M@sit.nyp.edu.sg>
2017-11-10 10:37:08 +0800

    MyFirstCommit

--- MathAdd.java
+++ MathAdd.java
@@ -4,5 +4,6 @@ public class MathAdd {
 	
 	static public int add(int a, int b) {   
         return a + b; 
+		System.out.println(a + b);
 	}
 }
---
+++ MathSub.java
@@ -0,0 +1,9 @@
+public class MathSub {
+	
+	public MathSub {}
+	
+	static public int sub(int a, int b) {   
+        return (a - b); 
+		System.out.println(a - b);
+	}
+}
